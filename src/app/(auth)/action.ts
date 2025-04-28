3c2dda7bb9f113f0e65afb93a2ad461532ba0ea8
HAMAD Ons <onshamadkl@gmail.com>
2025-04-28 10:47:38 +0100

    migration next 15.3

--- src/app/(auth)/action.ts
+++ src/app/(auth)/action.ts
@@ -4,7 +4,7 @@ import {signIn, signOut} from '@/auth'
 import {LoginFormSchema, RoleEnum, SignInError, SignupFormSchema} from '@/type'
 
 import {AuthError} from 'next-auth'
-import {isRedirectError} from 'next/dist/client/components/redirect'
+import {isRedirectError} from 'next/dist/client/components/redirect-error'
 import {getUserByEmailDao} from '../(dashboard)/shop-admin/actions'
 import {encrypt, hashPassword} from '@/crypt'
 import {CreateUser, users} from '@/db/schema/users'
